44990f6cf3e12dfddb6aa457a52345f50329dd04
Neel <styleneelpg@outlook.com>
2016-07-13 22:23:08 -0500

    Web Services - Xamarin Hands On Labs
    Web Service Con API REST

---
+++ WebApiXamarin/WebApiXamarin/Controllers/SeleccionController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApiXamarin.Models;
+
+namespace WebApiXamarin.Controllers
+{
+    public class SeleccionController : ApiController
+    {
+        // GET: Seleccion
+        BDEventoXamarinEntities bd = new BDEventoXamarinEntities();  // Instanciamos la base de datos
+
+        //Creamos un controlador del Tipo GET con el nombre GetJugador y el parámetro ID
+        public List<JugadorModel> GetJugador(int id)
+        {
+           // Realizamos la consulta a nuestra BD de la tabla Jugador
+
+            var query = (from J in bd.Jugador.ToList()
+                         where J.jugador_id == id
+                         select new JugadorModel()
+                         {
+                             //Almacenamos la informacion de nuestra consulta a nuestro modelo JugadorModel
+                             IDJugador = J.jugador_id,
+                             Nombre = J.jugador_nombre,
+                             Foto = J.jugador_foto,
+                             NumeroJugador = J.jugador_numero,
+                             Posicion = J.jugador_posicion,
+                             EquipoJugador = J.jugador_equipo
+                         });
+            return query.ToList();
+
+            // URL: http://localhost:{Puerto}/api/Seleccion/1 
+        }
+
+        //Creamos un controlador del Tipo GET con el nombre GetJugadores
+        public List<EquipoModel> GetJugadores()
+        {
+            // Realizamos la consulta a nuestra BD de la tabla Equipo
+            var query = (from E in bd.Equipo.ToList()
+                         select new EquipoModel
+                         {
+                             //Almacenamos la informacion de nuestra consulta a nuestro modelo EquipoModel
+                             IDEquipo = E.equipo_id,
+                             Nombre = E.equipo_nombre,
+                             Copas = E.equipo_copas,
+                             Entrenador = E.equipo_entrenador,
+                             Estadio = E.equipo_estadio,
+                             Fundacion = E.equipo_fundacion,
+                             JugadorModel = (from J in bd.Jugador.ToList() //Almacenamos la informacion de nuestra consulta a nuestro modelo JugadorModel
+                                             select new JugadorModel
+                                             {
+                                                 IDJugador = J.jugador_id,
+                                                 Nombre = J.jugador_nombre,
+                                                 Foto = J.jugador_foto,
+                                                 NumeroJugador = J.jugador_numero,
+                                                 Posicion = J.jugador_posicion,
+                                                 EquipoJugador = J.jugador_equipo
+                                             }).ToList()                        
+              
+                        }).ToList();
+
+            return query.ToList();
+
+            // URL: http://localhost:{Puerto}/Api/Seleccion
+        }
+    }
+}
---
+++ WebApiXamarin/WebApiXamarin/Models/EquipoModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using WebApiXamarin.Models;
+
+namespace WebApiXamarin.Controllers
+{
+    public class EquipoModel
+    {
+        public int IDEquipo { get; set; }
+        public string Nombre { get; set; }
+        public System.DateTime Fundacion { get; set; }
+        public string Entrenador { get; set; }
+        public string Estadio { get; set; }
+        public int Copas { get; set; }
+        public virtual ICollection<JugadorModel> JugadorModel { get; set; }
+    }
+}
